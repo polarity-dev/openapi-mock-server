a9571e30e5bbdb3eb169204a705ea7c7b48e60ec
Zhou Jian <zhoujian0998@gmail.com>
2020-12-30 15:37:48 +0100

    Add rollup-plugin-copy npm package

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from "@rollup/plugin-json"
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "rollup-plugin-typescript2"
 import resolve from "@rollup/plugin-node-resolve"
+import copy from "rollup-plugin-copy"
 import { terser } from "rollup-plugin-terser"
 
 export default [
@@ -14,6 +15,11 @@ export default [
     }],
     plugins: [
       json(),
+      copy({
+        targets: [
+          { src: "src/types", dest: "dist" }
+        ]
+      }),
       typescript({
         tsconfig: "./tsconfig.rollup.json"
       }),
